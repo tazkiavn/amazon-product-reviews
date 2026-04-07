a2c97ac96fe16828a5a47b4ccb651954b0bfd8c0
Tazkia Oktaviana Mardhotillah <tazkiavn29@gmail.com>
2026-04-07 22:56:29 +0700

    sentiment labeling

--- Main.py
+++ Main.py
@@ -6,6 +6,8 @@ data = pd.read_csv("Amazon.csv")
 data = data[['reviews.text','reviews.rating','brand','categories']]
 data = data.dropna()
 
+print(data.head())
+
 def clean_text(text):
     text = str(text)                       
     text = text.lower()                    
@@ -16,3 +18,15 @@ data['reviews.text'] = data['reviews.text'].astype(str)
 data['cleaned_reviews.text'] = data['reviews.text'].apply(clean_text)
 
 print(data['cleaned_reviews.text'])
+
+def sentiment_label(rating):
+    if rating > 4:
+        return "positive"
+    elif rating == 3:
+        return "neutral"
+    else:
+        return "negative"
+
+data['sentiment'] = data['reviews.rating'].apply(sentiment_label)
+
+print(data['sentiment'])
