fa25a0cfc7cbf6e220aa38829c192603eb7efd24
Tazkia Oktaviana Mardhotillah <tazkiavn29@gmail.com>
2026-04-07 22:50:29 +0700

    add data cleaning process

--- Main.py
+++ Main.py
@@ -1,8 +1,18 @@
 import pandas as pd
+import re
 
 data = pd.read_csv("Amazon.csv")
 
 data = data[['reviews.text','reviews.rating','brand','categories']]
 data = data.dropna()
 
-print(data)
+def clean_text(text):
+    text = str(text)                       
+    text = text.lower()                    
+    text = re.sub(r'[^a-zA-Z]', ' ', text) 
+    return text
+
+data['reviews.text'] = data['reviews.text'].astype(str)
+data['cleaned_reviews.text'] = data['reviews.text'].apply(clean_text)
+
+print(data['cleaned_reviews.text'])
